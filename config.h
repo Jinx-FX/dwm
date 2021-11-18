9421b78b17636c31277dee3a11e8999c6c466927
Jinx-FX <2674802603@qq.com>
2021-11-18 22:00:10 +0800

    my dwm build

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
@@ -15,10 +15,10 @@ static const unsigned int gappoh    = 10;       /* horiz outer gap between windo
 static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
-static const char *fonts[]          = { "SauceCodePro Nerd Font Mono:size=16" };
-static const char dmenufont[]       = "SauceCodePro Nerd Font Mono:size=16";
+static const char *fonts[]          = { "Source Code Pro Nerd Font Mono:size=14" };
+static const char dmenufont[]       = "Source Code Pro Nerd Font Mono:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -40,7 +40,8 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-//static const char *tags[] = { "一", "二", "三", "四", "五", "六", "七", "八", "九" };
+//需要 nerd-fonts 字体
+// static const char *tags[] = { "一", "二", "三", "四", "五", "六", "七", "八", "九" };
 static const char *tags[] = { "\uf120", "\uf7ae", "\uf121", "\uf04b", "\ue62e", "\uf251", "\ue727", "\uf537", "\uf684" };
 
 static const Rule rules[] = {
@@ -48,12 +49,12 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Android Emulator", NULL,       NULL,       0,            1,           -1 },
-	{ "Emulator", NULL,       NULL,       0,            1,           -1 },
-	{ "quemu-system-i386", NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class                instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",               NULL,       NULL,       0,            1,           -1 },
+	{ "Android Emulator",   NULL,       NULL,       0,            1,           -1 },
+	{ "Emulator", 			NULL,       NULL,       0,            1,           -1 },
+	{ "quemu-system-i386",  NULL,       NULL,       0,            1,           -1 },
+	{ "chromium",            NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
@@ -65,7 +66,7 @@ static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "Tile",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "[M]",      monocle }, //windows nums?
 };
 
 /* key definitions */
@@ -83,72 +84,77 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *browsercmd[]  = { "google-chrome-stable", NULL };
+static const char *browsercmd[]  = { "chromium", NULL };
+static const char *thunarcmd[]  = { "thunar", NULL };
+static const char *xfce4appfindercmd[]  = { "xfce4-appfinder", NULL };
+static const char *thunderbirdcmd[]  = { "thunderbird", NULL };
+static const char *steamcmd[]  = { "steam", NULL };
 
-static const char *upvol[]   = { "/home/david/scripts/vol-up.sh",  NULL };
-static const char *downvol[] = { "/home/david/scripts/vol-down.sh",  NULL };
-static const char *mutevol[] = { "/home/david/scripts/vol-toggle.sh",  NULL };
+static const char *upvol[]   = { "/home/july/scripts/vol-up.sh",  NULL };
+static const char *downvol[] = { "/home/july/scripts/vol-down.sh",  NULL };
 
-static const char *wpcmd[]  = { "/home/david/scripts/wp-change.sh", NULL };
-static const char *sktogglecmd[]  = { "/home/david/scripts/sck-tog.sh", NULL };
+static const char *wpcmd[]  = { "/home/july/scripts/wp-change.sh", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "80x24", NULL };
 
-static const char *setcolemakcmd[]  = { "/home/david/scripts/setxmodmap-colemak.sh", NULL };
-static const char *setqwertycmd[]  = { "/home/david/scripts/setxmodmap-qwerty.sh", NULL };
+static const char *suspendcmd[]  = { "/home/july/scripts/suspend.sh", NULL };
 
-static const char *suspendcmd[]  = { "/home/david/scripts/suspend.sh", NULL };
-
-static const char *screenshotcmd[] = { "flameshot", "gui", NULL };
 
 static Key keys[] = {
 	/* modifier            key                      function        argument */
-	{ MODKEY,              XK_s,                    spawn,          {.v = dmenucmd } },
+	//exe
+	{ MODKEY,              XK_d,                    spawn,          {.v = dmenucmd } },
 	{ MODKEY,              XK_Return,               spawn,          {.v = termcmd } },
-	{ MODKEY,              XK_c,                    spawn,          {.v = browsercmd } },
-	{ MODKEY|ShiftMask,    XK_w,                    spawn,          {.v = setqwertycmd } },
-	{ MODKEY|ShiftMask,    XK_m,                    spawn,          {.v = setcolemakcmd } },
-	{ MODKEY|ShiftMask,    XK_p,                    spawn,          {.v = suspendcmd } },
-	{ MODKEY|ControlMask,  XK_s,                    spawn,          {.v = sktogglecmd } },
-	{ 0,                   XF86XK_AudioLowerVolume, spawn,          {.v = downvol } },
-	{ 0,                   XF86XK_AudioMute,        spawn,          {.v = mutevol } },
-	{ 0,                   XF86XK_AudioRaiseVolume, spawn,          {.v = upvol   } },
-	{ MODKEY,              XK_bracketleft,          spawn,          {.v = downvol } },
-	{ MODKEY,              XK_backslash,            spawn,          {.v = mutevol } },
-	{ MODKEY,              XK_bracketright,         spawn,          {.v = upvol   } },
-	{ MODKEY,              XK_b,                    spawn,          {.v = wpcmd } },
-	{ 0,                   XK_Print,                spawn,          {.v = screenshotcmd } },
-	{ MODKEY|ShiftMask,    XK_e,                    rotatestack,    {.i = +1 } },
-	{ MODKEY|ShiftMask,    XK_u,                    rotatestack,    {.i = -1 } },
-	{ MODKEY,              XK_e,                    focusstack,     {.i = +1 } },
-	{ MODKEY,              XK_u,                    focusstack,     {.i = -1 } },
-	{ MODKEY,              XK_n,                    viewtoleft,     {0} },
-	{ MODKEY,              XK_i,                    viewtoright,    {0} },
-	{ MODKEY|ShiftMask,    XK_n,                    tagtoleft,      {0} },
-	{ MODKEY|ShiftMask,    XK_i,                    tagtoright,     {0} },
+	{ MODKEY|ControlMask,  XK_Return,       	    togglescratch,  {.v = scratchpadcmd } },
+	{ MODKEY|ShiftMask,    XK_Return,               zoom,           {0} },
+	{ MODKEY,			   XK_c,                    spawn,          {.v = browsercmd } },
+	{ Mod1Mask,            XK_b,                    spawn,          {.v = thunderbirdcmd } },
+	{ Mod1Mask,            XK_s,                    spawn,          {.v = steamcmd } },
+	{ Mod1Mask,            XK_t,                    spawn,          {.v = thunarcmd } },
+	{ Mod1Mask,            XK_f,                    spawn,          {.v = xfce4appfindercmd } },
+	//volume
+	{ MODKEY,              XK_Down,          		spawn,          {.v = downvol } },
+	{ MODKEY,              XK_Up,         			spawn,          {.v = upvol   } },
+	//wallpaper
+	{ MODKEY,              XK_a,                    spawn,          {.v = wpcmd } },
+	//window switch
+	{ MODKEY|ShiftMask,    XK_j,                    rotatestack,    {.i = +1 } },
+	{ MODKEY|ShiftMask,    XK_k,                    rotatestack,    {.i = -1 } },
+	{ MODKEY,              XK_j,                    focusstack,     {.i = +1 } },
+	{ MODKEY,              XK_k,                    focusstack,     {.i = -1 } },
+	//windwos 向 左||右 移动到其他 workspace
+	{ MODKEY|ControlMask,  XK_h,                    tagtoleft,      {0} },
+	{ MODKEY|ControlMask,  XK_l,                    tagtoright,     {0} },
+	//windwos 向 左||右 堆叠
 	{ MODKEY|ShiftMask,    XK_h,                    incnmaster,     {.i = +1 } },
 	{ MODKEY|ShiftMask,    XK_l,                    incnmaster,     {.i = -1 } },
+	//windwos 边界向 左||右 移动 
 	{ MODKEY,              XK_h,                    setmfact,       {.f = -0.05} },
 	{ MODKEY,              XK_l,                    setmfact,       {.f = +0.05} },
-	{ MODKEY,              XK_k,                    hidewin,        {0} },
-	{ MODKEY|ShiftMask,    XK_k,                    restorewin,     {0} },
-	{ MODKEY,              XK_o,                    hideotherwins,  {0}},
-	{ MODKEY|ShiftMask,    XK_o,                    restoreotherwins, {0}},
-	{ MODKEY|ShiftMask,    XK_Return,               zoom,           {0} },
-	{ MODKEY,              XK_Tab,                  view,           {0} },
+	//windwos 全屏
+	{ MODKEY, 			   XK_f,                    fullscreen,     {0} },
+	//hide and restore windows
+	{ MODKEY,              XK_n,                    hidewin,        {0} },
+	{ MODKEY|ShiftMask,    XK_n,                    restorewin,     {0} },
+	{ MODKEY,              XK_m,                    hideotherwins,  {0}},
+	{ MODKEY|ShiftMask,    XK_m,                    restoreotherwins, {0}},
+	//关闭选中的窗口
 	{ MODKEY|ShiftMask,    XK_q,                    killclient,     {0} },
-	{ MODKEY,              XK_t,                    setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,              XK_m,                    setlayout,      {.v = &layouts[2]} },
-	{ MODKEY|ShiftMask,    XK_f,                    fullscreen,     {0} },
+	//改变窗口布局
+	{ MODKEY,              XK_z,                    setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,              XK_x,                    setlayout,      {.v = &layouts[2]} },
+	//切换窗口布局
 	{ MODKEY,              XK_space,                setlayout,      {0} },
+	// 讲变形的窗口恢复原状
 	{ MODKEY|ShiftMask,    XK_space,                togglefloating, {0} },
-	{ MODKEY,              XK_apostrophe,           togglescratch,  {.v = scratchpadcmd } },
-	{ MODKEY,              XK_0,                    view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,    XK_0,                    tag,            {.ui = ~0 } },
+
+	// 未发现的功能
 	{ MODKEY,              XK_comma,                focusmon,       {.i = -1 } },
 	{ MODKEY,              XK_period,               focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,    XK_comma,                tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,    XK_period,               tagmon,         {.i = +1 } },
+
+	//workspace swicth
 	TAGKEYS(               XK_1,                      0)
 	TAGKEYS(               XK_2,                      1)
 	TAGKEYS(               XK_3,                      2)
@@ -158,7 +164,19 @@ static Key keys[] = {
 	TAGKEYS(               XK_7,                      6)
 	TAGKEYS(               XK_8,                      7)
 	TAGKEYS(               XK_9,                      8)
-	{ MODKEY|ControlMask,  XK_q,      quit,           {0} },
+	//合并在一起观看
+	{ MODKEY,              XK_0,                    view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,    XK_0,                    tag,            {.ui = ~0 } },
+	//向 左||右 浏览 workspace,一一浏览，包括未使用的workspace
+	{ MODKEY,              XK_i,                    viewtoleft,     {0} },
+	{ MODKEY,              XK_o,                    viewtoright,    {0} },
+	//在最近 2 个workspace 切换 
+	{ MODKEY,              XK_Tab,                  view,           {0} },
+	//system sleep
+	{ MODKEY|ShiftMask,    XK_p,                    spawn,          {.v = suspendcmd } },
+	//system exit
+	{ MODKEY|ControlMask,  XK_e,                    quit,           {0} },
+
 };
 
 /* button definitions */
